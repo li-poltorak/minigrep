29786492b3e4e41b3c41720a0bd4a4150ad9dbff
Emyli Poltorak <emylipoltorak@gmail.com>
2018-09-29 21:15:41 -0700

    added tests and case insensitive search with an environment variable

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate minigrep;
+
 use std::env;
-use std::fs;
 use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,29 +16,5 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
 
-    run(config);
-}
-
-fn run(config: Config) {
-    let contents = fs::read_to_string(config.filename)
-        .expect("Something went wrong reading the file");
-    
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config {query, filename})
-    }
+    minigrep::run(config);
 }
